b2b1e403b3174eb36fa051a79e69796c7497db37
Taqkun13 <99624594+Taqkun13@users.noreply.github.com>
2022-02-17 14:20:40 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,17 +1,46 @@
 <?php
-//get data from form  
-
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "th596540@gmail.com";
-$subject = "Mail From Dayat.web";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@Dayat.github.io" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+$statusMsg = '';
+$msgClass = '';
+if(isset($_POST['submit'])){
+    // Get the submitted form data
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    
+    // Cek apakah ada data yang belum diisi
+    if(!empty($email) && !empty($name) && !empty($message)){
+        
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+            $msgClassk = 'errordiv';
+        }else{
+            // Pengaturan penerima email dan subjek email
+            $toEmail = 'th596540@gmail.com'; // Ganti dengan alamat email yang Anda inginkan
+            $emailSubject = 'Pesan website dari '.$name;
+            $htmlContent = '<h2> via Form Kontak Website</h2>
+                <h4>Name</h4><p>'.$name.'</p>
+                <h4>Email</h4><p>'.$email.'</p>
+                <h4>Message</h4><p>'.$message.'</p>';
+            
+            // Mengatur Content-Type header untuk mengirim email dalam bentuk HTML
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            
+            // Header tambahan
+            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
+            
+            // Send email
+            if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
+                $statusMsg = 'Pesan Anda sudah terkirim dengan sukses !';
+                $msgClass = 'succdiv';
+            }else{
+                $statusMsg = 'Maaf pesan Anda gagal terkirim, silahkan ulangi lagi.';
+                $msgClass = 'errordiv';
+            }
+        }
+    }else{
+        $statusMsg = 'Harap mengisi semua field data';
+        $msgClass = 'errordiv';
+    }
 }
-//redirect
-header("Location:thankyou.html");
 ?>
